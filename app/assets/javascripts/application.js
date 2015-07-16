08a14b908352e935ce7b167c9c194ebd4c247d51
Bartosz Dyja <bartosz.dyja@gmail.com>
2015-07-16 23:46:26 +0200

    Added ded Devise

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,15 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require materialize
 //= require turbolinks
 //= require_tree .
+
+  $(document).ready(function(){
+    $('.collapsible').collapsible({
+      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
+    $(".button-collapse").sideNav();
+  });
+
+        
